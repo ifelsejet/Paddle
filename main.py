3c075c8b159d5e2fe9904f896f8b40b742c4b2d2
Jatnael Montes <ifelsejet@gmail.com>
2019-07-29 09:45:22 -0700

    Created the JoinEvent handler and html page

--- main.py
+++ main.py
@@ -19,9 +19,17 @@ class MainPage(webapp2.RequestHandler):
         template = jinja_env.get_template("templates/main.html")
         self.response.write(template.render())
 
+class JoinEventPage(webapp2.RequestHandler):
+    def get(self): #for a get request
+
+        #Step 3: Use the Jinja environment to get our HTML
+        template = jinja_env.get_template("templates/joinEvent.html")
+        self.response.write(template.render())
+
 # the app configuration section
 app = webapp2.WSGIApplication([
     ('/', MainPage), #this maps the root url to the Main Page Handler
+    ('/joinEvent' , JoinEventPage),
 
 
 ], debug=True)
