a136156458fe8805debda0941ca0c9735dbafb82
Jatnael Montes <ifelsejet@gmail.com>
2019-07-31 11:55:42 -0700

    Fixed all errors, consider this the working version
    Git hurts

--- main.py
+++ main.py
@@ -62,49 +62,6 @@ class CreateAccount(webapp2.RequestHandler):
 
         self.redirect("/main", True)
 
-class SignIn_Transition(webapp2.RequestHandler):
-    def get(self):
-        #Python API Notes
-        '''
-        Set up my key (in GCP console) and url
-        api_key = 'something1234567'
-        params = {'q': 'Harry Potter',
-        'api_key': api_key}
-        base_url = 'url'
-        full_url = base_url + '?' + urllib.urlencode(params)
-
-        Fetch the url (service name [in GCP])
-        response  = urlfetch.fetch(full_url).content
-
-
-        Get the JSON response and convert to dictionary
-        response_dictonary = json.loads(response)
-
-        template_vars = {
-        'books': books_dictionary['items'],
-        }
-        '''
-        #if email is in datastore contue to Main Page w list of joinEvent
-        #creting a list that stores all the emails in datastore
-        #user.email() represents the email of the user that just logged in according to
-        #api docs
-        #Profile.email() represents the email attributes assoiated with User models in datastore
-        user = users.get_current_user()
-        signin_link = users.create_login_url('/')
-
-        email_address = user.email()
-        email_match_value = Profile.query().filter(Profile.email == email_address).get()
-        #creating an if statment that checks if the email used to \\
-        #login is already in datastore
-        if email_match_value:
-            self.redirect("/main", True)
-            #created a sign out link
-            #the structure of the main page
-        #redirect user to make account page
-        #where the create a user model with their email and extra info
-        else:
-            self.redirect("/createaccount", True)
-
 class Main(webapp2.RequestHandler):
     # need to do queryof Event datastore
     def get(self): #for a get request
@@ -123,25 +80,6 @@ class Main(webapp2.RequestHandler):
         }
         template = jinja_env.get_template("templates/main.html")
         self.response.write(template.render(template_vars))
-
-
-class JoinEventPage(webapp2.RequestHandler):
-    def get(self): #for a get request
-        #Step 3: Use the Jinja environment to get our HTML
-        template = jinja_env.get_template("templates/joinEvent.html")
-        self.response.write(template.render())
-
-class AboutPage(webapp2.RequestHandler):
-    def get(self): #for a get request
-        #Step 3: Use the Jinja environment to get our HTML
-        template = jinja_env.get_template("templates/about.html")
-        self.response.write(template.render())
-
-class FlexBoxPage(webapp2.RequestHandler):
-    def get(self): #for a get request
-        #Step 3: Use the Jinja environment to get our HTML
-        template = jinja_env.get_template("templates/flexboxTest.html")
-        self.response.write(template.render())
 
 class CreateNewEventPage(webapp2.RequestHandler):
     def get(self):
@@ -183,14 +121,6 @@ class JoinEventPage(webapp2.RequestHandler):
     def get(self):
         template = jinja_env.get_template("templates/joinEvent.html")
         self.response.write(template.render())
-
-class Main(webapp2.RequestHandler):
-        def get(self):
-            template_vars = {
-            'logout_link' : users.create_logout_url('/')
-            }
-            template = jinja_env.get_template("templates/main.html")
-            self.response.write(template.render(template_vars))
 
 class SignIn_Transition(webapp2.RequestHandler):
     def get(self):
